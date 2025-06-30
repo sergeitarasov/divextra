41f09d8f647122db19221dd6fde3f789670d53eb
Sergei Tarasov <sergxf@yandex.ru>
2025-06-30 14:55:17 +0300

    along branch

--- R/asr.R
+++ R/asr.R
@@ -1,14 +1,5 @@
 
 
-# diversitree:::make.asr.marginal.geosse
-
-
-# lik <- lik.td
-# make.asr.marginal.classe(lik.td)(par.td)
-# asrm <- make.asr.marginal.classe(lik.td)
-# st <- asrm(par.td)
-
-
 make.do.asr.marginal_divextra <- function(all_branches, rootfunc)
 {
   eb <- environment(all_branches)
@@ -50,6 +41,9 @@ make.asr.marginal.classe <- function (lik, ...)
 
 
 
+asr.marginal.classe <- function (lik, pars, nodes = NULL, ...) 
+  make.asr.marginal.classe(lik)(pars, nodes, ...)
+
 
 #-----------  ASR along branch
 
@@ -67,13 +61,17 @@ do.asr.marginal.R_branch <- function(pars, cache, res, nodes, states.idx,
   root.idx <- cache$root
   anc <- cache$ancestors
 
-  if ( is.null(nodes) )
-    nodes <- root.idx:max(cache$order)
-  else
-    nodes <- nodes + cache$n.tip
+  # Check that nodes length should be one
+  if (length(nodes) != 1) {
+    stop("Provide a node defining a branch!")
+  }
+  # nodes <- nodes + cache$n.tip
+  nd <- nodes + cache$n.tip
+  cat('Working on a branch of the node: ', nd, '\n')
 
-  # nd <- 9
-  f <- function(nd) {
+# nd <- 9
+#f <- function(nd) {
+    cat('##################---- Node: ', nd, '\n')
     ## Include current node but omit root:
     anc.nd <- c(nd, anc[[nd]])
     anc.nd <- anc.nd[-length(anc.nd)]
@@ -81,40 +79,88 @@ do.asr.marginal.R_branch <- function(pars, cache, res, nodes, states.idx,
 
     # st=1
     for (st in seq_along(states.idx) ) {
+      cat('---- st: ', st, '\n')
       lq <- res$lq
       branch.init <- res$init
       branch.base <- res$base
+      # !!
       # branch.init[states.idx[-st],nd] <- 0
       y.in <- branch.init[,nd]
+      cat('y.in <- branch.init[,nd]: ', y.in, '\n')
 
       # i=9
-      for ( i in anc.nd ) {
-        # there two ans (from 0 to relative.t.branch) and (from  relative.t.branch to len[i])
-        # The Ancestral Probs are at relative.t.branch
-        # ans <- branches(y.in, len[i], pars, depth[i], i)
-        # relative.t.branch <- .01
-        ans_1 <- branches(y.in, relative.t.branch, pars, depth[i], i)
-        ans_1.lq <- ans_1[[1]]
-        ans_1.base <- ans_1[[2]]
+      #for ( i in anc.nd ) {
+      for (index in 1:length(anc.nd) ) {
+        i <- anc.nd[index]
+
+        cat('---- Ancestral Nodes, anc.nd i: ', i, '\n')
+        # the first branch is the focal one
+        # so ASR calculation is different
+        if (index==1){
+          # we hame multiple IFs: for numeric and "BASE"
+          if (is.numeric(relative.t.branch)){
+            # there two ans (from 0 to relative.t.branch) and (from  relative.t.branch to len[i])
+            # The Ancestral Probs are at relative.t.branch
+            # ans <- branches(y.in, len[i], pars, depth[i], i)
+            ans_test <- branches(y.in, len[i], pars, depth[i], i)
+            cat('depth[i]: ', depth[i], '\n')
+            print('ans_test:')
+            print(ans_test)
+            # relative.t.branch <- .01
+            cat('relative.t.branch: ', relative.t.branch, '\n')
+            ans_1 <- branches(y.in, relative.t.branch, pars, depth[i], i)
+            #cat('ans_1: ',, '\n')
+            print('ans_1:')
+            print( ans_1)
+            #
+            ans_1.lq <- ans_1[[1]]
+            ans_1.base <- ans_1[[2]]
+            #
+            # y.in_2 is where Ancestral Probs are calculated
+            y.in_2 <- c(ans_1.base)
+            #-----
+            y.in_2[states.idx[-st]] <- 0
+            cat('y.in_2 <-0: ', y.in_2, '\n')
+            ans_2 <- branches(y.in_2, len[i] - relative.t.branch, pars, depth[i] + relative.t.branch, i)
+            # ans_2 <- branches(y.in_2, len[i] - relative.t.branch, pars, depth[i] + relative.t.branch, i)
+            #cat('ans_2: ', ans_2, '\n')
+            print('ans_2:')
+            print(ans_2)
+            #----
+            ans_2.lq <- ans_2[[1]]
+            ans_2.base <- ans_2[[2]]
+            # we need to sum lqs
+            ans_total.lq <- ans_1.lq + ans_2.lq
+            # lq[i] <- ans[[1]]
+            # branch.base[,i] <- ans[[2]]
+            lq[i] <- ans_total.lq
+            branch.base[,i] <- ans_2.base
+            #
+          } else if (relative.t.branch == "BASE") {
+            print("BASE")
+
+            branch.base[states.idx[-st],nd] <- 0
+            y.in <- branch.base[ ,nd]
+            print(y.in)
+            lq[i] <- log(sum(y.in))
+            print(lq[i])
+            #branch.base[,i] <- ans[[2]]
+
+          } else {
+            stop("relative.t.branch: arguments has wrong value")
+          }
+
+        } else {
+          ans <- branches(y.in, len[i], pars, depth[i], i)
+          lq[i] <- ans[[1]]
+          branch.base[,i] <- ans[[2]]
+        }
         #
-        # y.in_2 is where Ancestral Probs are calculated
-        y.in_2 <- c(ans_1.base)
-        y.in_2[states.idx[-st]] <- 0
-        ans_2 <- branches(y.in_2, len[i] - relative.t.branch, pars, depth[i] + relative.t.branch, i)
-        ans_2.lq <- ans_2[[1]]
-        ans_2.base <- ans_2[[2]]
-        # we need to sum lqs
-        ans_total.lq <- ans_1.lq + ans_2.lq
-
-        # lq[i] <- ans[[1]]
-        # branch.base[,i] <- ans[[2]]
-        lq[i] <- ans_total.lq
-        branch.base[,i] <- ans_2.base
-
         j <- parent[i]
         y.in <- initial.conditions(branch.base[,children[j,]], pars,
                                    depth[j], j)
         branch.init[,j] <- y.in
+        cat('branch.init[,j] <- y.in: ',  y.in, '\n')
       }
       ans <- root(list(vals=branch.init[,root.idx], lq=lq), pars)
       ## explots IEEE arithmetic's exp(-Inf) == 0
@@ -122,10 +168,11 @@ do.asr.marginal.R_branch <- function(pars, cache, res, nodes, states.idx,
     }
 
     pp <- exp(p - max(p))
-    pp/sum(pp)
-  }
+    #pp/sum(pp)
+    matrix(pp/sum(pp), ncol = 1)
+#}
 
-  matrix(unlist(lapply(nodes, f)), ncol=length(nodes))
+  #matrix(unlist(lapply(nodes, f)), ncol=length(nodes))
 }
 
 
--- asr/asr_toy.R
+++ asr/asr_toy.R
@@ -13,33 +13,42 @@ tiplabels()
 edgelabels()
 axisPhylo()
 
-lik.td <-make.classe.td(phy, phy$tip.state_3, k=3, n.epoch=2, control=list(backend = "gslode", tol=1e-16, eps=1e-4), strict=T)
+#tol=1e-16, eps=1e-4
+lik.td <-make.classe.td(phy, phy$tip.state_3, k=3, n.epoch=2,
+                        control=list(backend = "gslode"), strict=T)
 par.td <- c(4, c(1:54)/100)
 names(par.td) <- diversitree::argnames(lik.td)
-lik.est <- lik.td(par.td, root=ROOT.FLAT, intermediates=T)
-print(lik.est)
+lik.est <- lik.td(par.td, intermediates=T)
+#print(lik.est)
 
 #---- ASR
-make.asr.marginal.classe(lik.td)(par.td, nodes=4)
-st <- make.asr.marginal.classe(lik.td)(par.td)
+# make.asr.marginal.classe(lik.td)(par.td, nodes=4)
+st <- make.asr.marginal.classe(lik.td)(par.td, root=ROOT.GIVEN, root.p=c(1,0,0))
 st
-cols <- mapvalues(phy$tip.state_3, from = c(1:3), to=c('black', "blue","red" ))
-plot(phy, label.offset = 2, cex=1, no.margin = F)
-tiplabels(pch = 15, col = cols, cex = 2, adj = 1.5)
-nodelabels(thermo=t(st), piecol=c('black', "blue","red"), cex=2, adj=.5)
+# cols <- mapvalues(phy$tip.state_3, from = c(1:3), to=c('black', "blue","red" ))
+# plot(phy, label.offset = 2, cex=1, no.margin = F)
+# tiplabels(pch = 15, col = cols, cex = 2, adj = 1.5)
+# nodelabels(thermo=t(st), piecol=c('black', "blue","red"), cex=2, adj=.5)
 
-getMRCA(phy, c(1,2))
+
 
 #---- ASR along branch
-node.plot <- 9
+node.plot <- 7
 node.id <- node.plot - 5
 node.id
 #relative.t.branch
 source('R/asr.R')
-phy$edge.length # 5.564629
-make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id, relative.t.branch= 0.001)
-# 0.1822480
+phy$edge.length # 5.564629 # depth: 1.502053
+make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id, relative.t.branch= 0, root=ROOT.GIVEN, root.p=c(1,0,0))
+make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id, relative.t.branch= 5.564)
+make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id, relative.t.branch= phy$edge.length[1])
+make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id, relative.t.branch= "BASE")
+st
 
+# [,1]
+# [1,] 0.3646077
+# [2,] 0.3308331
+# [3,] 0.3045592
 #--- ASR along branch
 Nbins <- 10
 lik <- lik.td
@@ -48,7 +57,7 @@ e <- environment(lik)
 eb <- environment(e$all_branches)
 cache <- eb$cache
 
-node.plot <- 8 # upstream node
+node.plot <- 9 # upstream node
 br.len <- cache$len[node.plot]
 node.id.diversitree <- node.plot - Ntip(phy)
 
@@ -57,7 +66,8 @@ time.bins <- seq(0+eps, br.len-eps, length.out=Nbins)
 ans.t <- c()
 for (t in time.bins){
   print(t)
-  x <- make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id.diversitree, relative.t.branch= t)
+  x <- make.asr.marginal.classe_branch(lik.td)(par.td, nodes=node.id.diversitree,
+                                               relative.t.branch= t, root=ROOT.GIVEN, root.p = c(1,0,0))
   ans.t <- cbind(ans.t, x)
 }
 
@@ -74,6 +84,42 @@ plot(time.bins, ans.t[3,], type='l')
 # root.idx <- cache$root
 # anc <- cache$ancestors
 
+#---- ASR along branch. Markdown
 
+st <- make.asr.marginal.classe(lik.td)(mle.td$par.full)
+#st
+#round(st, 2)
+round(st[,1:10], 2)
 
+cols <- mapvalues(phy$tip.state, from = c(1:3), to=c('orange', "green","red" ))
+plot(phy, show.tip.label = F, cex=1, no.margin = F)
+nodelabels(cex=.3)
+tiplabels(pch = 15, col = cols, cex = .5, adj = 1)
+nodelabels(thermo=t(st), piecol=c('orange', "green","red" ), cex=.3, adj=0)
+axisPhylo()
 
+Nbins <- 10
+lik <- lik.td
+eps <- 0.01
+e <- environment(lik)
+eb <- environment(e$all_branches)
+cache <- eb$cache
+
+node.plot <- 801 # upstream node
+br.len <- cache$len[node.plot]
+node.id.diversitree <- node.plot - Ntip(phy)
+
+time.bins <- seq(0+eps, br.len-eps, length.out=Nbins)
+
+ans.t <- c()
+for (t in time.bins){
+  print(t)
+  x <- make.asr.marginal.classe_branch(lik.td)(mle.td$par.full, nodes=node.id.diversitree,
+                                               relative.t.branch= t)
+  ans.t <- cbind(ans.t, x)
+}
+
+st[,node.id.diversitree]
+st[,729-Ntip(phy)]
+ans.t
+plot(time.bins, ans.t[3,], type='l')
